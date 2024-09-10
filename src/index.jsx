a5e82bf8ff23ae284e1dc726a9850d03fc3f4fc7
mateolopz <88870859+mateolopz@users.noreply.github.com>
2024-09-10 10:41:07 -0500

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Post from "./Post";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+ <Container className="mt-3">
+   <Row>
+     <Post author="John Doe" content="This is the post content" likes={20} />
+     <Post author="Anne Hill" content="I like React" likes={3} />
+     <Post author="Laia Martins" content="I love JSX" likes={0} />
+   </Row>
+ </Container>
+);
